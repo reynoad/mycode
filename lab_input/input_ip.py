096ec8f575bdb7652e127ca0cfa312170014fedd
reynoad <adam.reynolds@verizonwireless.com>
2018-12-03 14:59:31 -0500

    Lab 7

---
+++ lab_input/input_ip.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+#Written By: Adam Reynolds
+#Collect IP address and display it back.
+
+##Collect IP Address from user
+user_input = input('Please enter an IPv4 IP address:')
+
+##Print out the input from the user
+print("You told me the IPv4 address is:" + user_input)
+
+##Collect Vendor Name
+user_input = input('Please enter the Vendor name:')
+
+##Print Vendor Name
+print("Vendor Name:" + user_input)

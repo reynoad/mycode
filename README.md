110539a99429e7f7fe47d64e4cb84a4f9f152632
reynoad <adam.reynolds@verizonwireless.com>
2018-12-04 10:18:37 -0500

    2nd attempt to version control

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 Learning Version Control
 
-# mycode (Project Title)
+# mycode (Project Title) this is a test
 
 One Paragraph of your project description goes here. Describe what you're trying to do. What is the purpose of putting up this repo?
 
--- moveplease01.py
+++ moveplease01.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python3
 
+#Import shell utilities
 import shutil
 import os
 
+#Using the os module and the chdir method
 os.chdir('/home/student/mycode/')
+
+#Using shutil and move
 shutil.move('raynor.obj', 'ceph_storage/')
+
+#Prompt the user for a new name
 xname = input('What is the new name for kerrigan.obj? ')
+
+#Move the kerrigan.obj
 shutil.move('ceph_storage/kerrigan.obj', 'ceph_storage/' + xname)

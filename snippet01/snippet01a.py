1fdcdc40ee02055127b238b4a5b8b456bffc52d3
reynoad <adam.reynolds@verizonwireless.com>
2018-12-07 11:17:34 -0500

    join

---
+++ snippet01/snippet01a.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+file = open('Hunter_Wife.txt', 'r')
+print( 'File Name:', file.name )
+print( 'File Open Mode:', file.mode )
+
+text = file.readlines()
+print("\n\n".join(text))
